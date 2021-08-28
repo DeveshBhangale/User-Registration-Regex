fee946c7511de5ab9ab58504cd5097049bd529f7
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-28 08:51:10 +0530

    [Devesh]: added Validations for LastName

--- src/com/bridgelabz/UserRegistration/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration/UserRegistration.java
@@ -2,43 +2,78 @@ package com.bridgelabz.UserRegistration;
 import java.util.*;
 
 public class UserRegistration {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws Exception {
 		Scanner sc = new Scanner(System.in);
 		int a;
-		String input;
-		System.out.println("Enter Your Choice\n"
-				+ "1. First Name\n"
-				+ "2. Last Name\n"
-				+ "3. Email\n"
-				+ "4. Mobile\n"
-				+ "5. Password\n");
-		do {
-			System.out.println("Please enter a Number");
-			while(!sc.hasNextInt()) {
-				System.out.println("Entered Input is not a number");
-				sc.next();
-			}
-			a = sc.nextInt();
-			break;
-		}while(true);
-		
-		
-		switch(a) {
-			//Use Case 1
-			case 1: System.out.println("Please enter your first Name");
-					input = sc.next();
-					String Regex = "^[A-Z][a-z]{3,}";
-					if(input.matches(Regex))
-						System.out.println("First Name is Valid");
-					else
-						System.out.println("First Name is not Valid");
-					break;
+		String input,dummy;
+		while(true) {
 					
-			
+					System.out.println("\nEnter Your Choice\n"
+							+ "1. First Name\n"
+							+ "2. Last Name\n"
+							+ "3. Email\n"
+							+ "4. Mobile\n"
+							+ "5. Password\n");
+					do {
+						System.out.println("Please enter a Number");
+						while(!sc.hasNextInt()) {
+							System.out.println("Entered Input is not a number");
+							sc.next();
+						}
+						a = sc.nextInt();
+						break;
+					}while(true);
+					
+					
+					switch(a) {
+						//Use Case 1
+						case 1: System.out.println("Please enter your first Name");
+								input = sc.next();
+								String Regex = "^[A-Z][a-z]{2,}";
+								if(input.matches(Regex))
+									System.out.println("First Name is Valid");
+								else
+									System.out.println("First Name is not Valid");
+								break;
+								
+						//Use Case 2
+						case 2: System.out.println("Please enter your Last Name");
+								input = sc.next();
+								Regex = "^[A-Z][a-z]{2,}";
+								if(input.matches(Regex))
+									System.out.println("Last Name is Valid");
+								else
+									System.out.println("Last Name is not Valid");
+								break;
+								
+//						// Use Case 3
+//						case 3: System.out.println("Enter your Valid Email");
+//								input = sc.next();
+//								Regex = "^[a-zA-Z]{2,}?[.+-]?[0-9a-zA-Z]{0,}@[a-zA-Z0-9]{1,}.[a-z]{2,}?[.][a-z]{1,}";
+//								if(input.matches(Regex))
+//									System.out.println("Email is Valid");
+//								else
+//									System.out.println("Email is not Valid");
+//								break;
+//								
+//								
+//						// Use Case 4
+//						case 4: System.out.println("Enter your Valid Phone No.");
+//								dummy = sc.nextLine();
+//								input = sc.nextLine();
+//								Regex = "^[0-9]{1,3}\s[0-9]{10}";
+//								if(input.matches(Regex))
+//									System.out.println("Last Name is Valid");
+//								else
+//									System.out.println("Last Name is not Valid");
+//								break;
+						
+						default: System.out.println("Program Exited");return;
+								
+						
+					}
+				}
 		}
-		
-
-	}
 
 }
